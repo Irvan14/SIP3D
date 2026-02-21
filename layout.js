09fed2417c5af12dc9215cae8c27d21cb19eab82
Irvan14 <udienirvan@gmail.com>
2026-02-22 03:53:44 +0700

    Add files via upload

--- layout.js
+++ layout.js
@@ -12,3 +12,22 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.add("sidebar-collapsed");
   }
 });
+
+function toggleProfile(e) {
+  e.stopPropagation();
+  document.getElementById("profileMenu")
+    .classList.toggle("active");
+}
+
+document.addEventListener("click", function () {
+  document.getElementById("profileMenu")
+    ?.classList.remove("active");
+});
+
+// Close dropdown when click outside
+document.addEventListener("click", function (e) {
+  const dropdown = document.querySelector(".profile-dropdown");
+  if (!dropdown.contains(e.target)) {
+    document.getElementById("profileMenu")?.classList.remove("active");
+  }
+});
